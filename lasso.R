d1caabecec3b262a9f6fd998213db2e57ae6790d
louisdv75 <louisdevitry@gmail.com>
2017-04-13 09:15:04 +0200

    Lasso and Ridge Regression
    I uploaded two scripts, performing both a Lasso and Ridge Regression shrinkage.
    I will transform them into functions, that will be applied to the latest model in final_script.R

---
+++ lasso.R
@@ -0,0 +1,19 @@
+# Lasso
+x=model.matrix(SalePrice~.,train)[,-1]
+y=train$SalePrice
+grid=10^seq(10,-4,length=100)
+
+lasso.mod=glmnet(x[train_set ,],y[train_set],alpha=1,lambda=grid) 
+plot(lasso.mod)
+
+set.seed(1)
+
+cv.out=cv.glmnet(x[train_set ,],y[train_set],alpha=1)
+
+plot(cv.out,main="Lasso cross-validation")
+
+bestlam=cv.out$lambda.min
+
+lasso.pred=predict(lasso.mod,s=bestlam ,newx=x[test_set,])
+
+mean((lasso.pred-y.test)^2)
