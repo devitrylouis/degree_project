46e1e2feb0d937694a9cd844ccbdf23c7e41a19e
louisdv75 <your@email.com>
2017-04-13 04:04:17 +0200

    I added a function computing the MSE with 10-fold CV that plots: - MSE - Adjusted R-squared - Multiple R-squared

--- final_script.R
+++ final_script.R
@@ -1,6 +1,6 @@
 # Necessaries libraries to explore the data
 
-	load.libraries <- c('data.table', 'testthat', 'gridExtra', 'corrplot', 'GGally', 'ggplot2', 'e1071', 'dplyr','leaps','car','MASS','gtools')
+	load.libraries <- c('data.table', 'testthat', 'gridExtra', 'corrplot', 'GGally', 'ggplot2', 'e1071', 'dplyr','leaps','car','MASS','gtools','DAAG')
 	install.lib <- load.libraries[!load.libraries %in% installed.packages()]
 	for(libs in install.lib) install.packages(libs, dependences = TRUE)
 	sapply(load.libraries, require, character = TRUE)
@@ -15,6 +15,7 @@
 	source('~/Documents/Education/KTH/Bachelor thesis/Bachelor Thesis/quadratic.R')
 	source('~/Desktop/quadratic_transform.R')
 	source('~/Documents/Education/KTH/Bachelor thesis/Bachelor Thesis/remove_neighborhood.R')
+	source('~/Documents/Education/KTH/Bachelor thesis/Bachelor Thesis/plot_comparison.R')
 	
 # Importation of training and test set
 
@@ -24,30 +25,30 @@
 # Choose neighborhoods of interest	
 
 		neigh<-FALSE
-		
-  	if(neigh)
-  	{
-  	  train<-removing_neighborhoods(train)
-  	}
+  	if(neigh){train<-removing_neighborhoods(train)}
 	
 # Linear Model
-	
+
 	# Multiple Linear Model
 		
   	model<-fitting(train)
   	output<-analysis(df,model,residuals=TRUE,boxcox=TRUE,multicollinearity=TRUE,outliers=TRUE,high_terms=FALSE)
     
-	  # Transformed Multiple Linear Model
-	  
-    	model_transformed <- transformed(model,output,df,1)
-      output_transformed<-analysis(df1,model_transformed,residuals=TRUE,boxcox=FALSE,multicollinearity=TRUE,outliers=FALSE,high_terms=TRUE)
-      
-    # Multiple Linear Model with higher terms of interest
+  # Transformed Multiple Linear Model
   
-      model_squared <- quadratic_transform(output_transformed,df1)
-      output_squared <- analysis(data_squared,model_squared,residuals=TRUE,boxcox=FALSE,multicollinearity=TRUE,outliers=TRUE,high_terms=FALSE)
+  	model_transformed <- transformed(model,output,df,1)
+    output_transformed<-analysis(df1,model_transformed,residuals=TRUE,boxcox=FALSE,multicollinearity=TRUE,outliers=FALSE,high_terms=TRUE)
+    
+  # Multiple Linear Model with higher terms of interest
 
-    # Transformed Multiple Linear Model with higher terms
-      
-      model_squared_transformed <- transformed(model_squared,output_squared,data_squared,2)
-      output<-analysis(df2,model_squared_transformed,residuals=TRUE,boxcox=FALSE,multicollinearity=TRUE,outliers=FALSE,high_terms=FALSE)
+    model_squared <- quadratic_transform(output_transformed,df1)
+    output_squared <- analysis(data_squared,model_squared,residuals=TRUE,boxcox=FALSE,multicollinearity=TRUE,outliers=TRUE,high_terms=FALSE)
+
+  # Transformed Multiple Linear Model with higher terms
+    
+    model_squared_transformed <- transformed(model_squared,output_squared,data_squared,2)
+    output<-analysis(df2,model_squared_transformed,residuals=TRUE,boxcox=FALSE,multicollinearity=TRUE,outliers=FALSE,high_terms=FALSE)
+    
+  # Plot of the models assessment
+    
+    plot_comparison()
